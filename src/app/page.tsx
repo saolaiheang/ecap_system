e78b13bb40445f25a345ca56e1a4e8d60b04b854
sornmonika <monika.sorn@institute.pse.ngo>
2025-05-12 08:15:06 +0700

    admin panel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@ import Login from "./pages/login/page"
 import Homepage from "./pages/home/page";
 import Aboutpage from "./pages/about/page";
 import FootballDashboard from "@/components/footballProfile";
+import Typeofsport from "@/components/fetchTypeSport";
 
 
 export default function Home() {
   return (
     <div>
-      <Login/>
+      <Typeofsport/>
     </div>
   );
 }
